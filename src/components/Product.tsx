dae73b1cf2f21c3922007501966996b19fc6a386
puncoviy <usaspinner@gmail.com>
2022-09-24 18:50:08 +0300

    feat: add basic card styles, add button for details support

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IProduct } from '../models'
 
 interface ProductProps {
     product: IProduct
 }
 
-export function Product(props: ProductProps) {
-    props.product.rating.count
-  return (
-    <div
-        className='border py2 px4 rounded flex flex-col items-center mb-2'
-    >{props.product.title}</div>
-  )
+export function Product({ product }: ProductProps) {
+    const [details, setDetails] = useState(false)
+
+    return (
+        <div className='
+        border
+        py-2
+        px-4
+        rounded
+        flex
+        flex-col
+        items-center
+        mb-2'>
+            <img className='w-1/6' src={product.image} alt={product.title} />
+            <p>{product.title}</p>
+            <p className="font-bold">£{product.price}</p>
+            <button
+                className='py-2 px-4 border bg-yellow-400'
+                onClick={() => setDetails(true)}
+            >Show details</button>
+
+            {details && <div>
+                <p>{product.description}</p>
+                <button
+                    className='py-2 px-4 border bg-yellow-400'
+                    onClick={() => setDetails(false)}
+                >Hide details</button>
+            </div>}
+        </div>
+    )
 }
