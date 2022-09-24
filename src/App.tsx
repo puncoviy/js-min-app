1c23419890e28ab73a491326891986a5f1d9054f
puncoviy <usaspinner@gmail.com>
2022-09-24 19:38:32 +0300

    feat: add axios support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
 import { products } from './data/products';
+import axios from 'axios'
+import { IProduct } from './models';
 
 function App() {
+    async function fetchProducts() {
+        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+        console.log(response)
+    }
+
+    useEffect( () => {
+        fetchProducts()
+    }, [])
+
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            { products.map(product => <Product product={product} key={`item${Math.random()}`} />) }
+            { products.map(product => <Product product={product} key={`item${product.id}`} />) }
         </div>
     )
 }
