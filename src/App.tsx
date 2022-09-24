4b934330d8c27b0b8ffb4218e95ad569eab99d83
puncoviy <usaspinner@gmail.com>
2022-09-24 19:56:47 +0300

    feat: add loading and error notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product';
 // import { products } from './data/products';
-import axios from 'axios'
+import axios, {AxiosError} from 'axios'
 import { IProduct } from './models';
 
 function App() {
     const [products, setProducts] = useState<IProduct[]>([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
 
     async function fetchProducts() {
-        const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-        setProducts(response.data)
+        try {
+            setError('');
+            setLoading(true)
+            const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+            setProducts(response.data)
+            setLoading(false)
+        } catch (e: unknown) {
+            const error = e as AxiosError
+            setLoading(false)
+            setError(error.message)
+        }
     }
 
     useEffect( () => {
@@ -18,6 +29,8 @@ function App() {
 
     return (
         <div className="container mx-auto max-w-2xl pt-5">
+            {loading && <p className='text-center'>Loading...</p>}
+            {error && <p className='text-center text-red-600'>{error}</p>}
             { products.map(product => <Product product={product} key={`item${product.id}`} />) }
         </div>
     )
