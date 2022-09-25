5f42d5b2b483e7c8fef60e854d742e3f425d1de1
puncoviy <usaspinner@gmail.com>
2022-09-25 20:44:45 +0300

    feat: add title trim validation scritp; ui product add in progress

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,10 +1,32 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { IProduct } from "../models";
+import { ErrorMessage } from "./ErrorMessage";
+
+const productData: IProduct = {
+    title: 'test product',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+        rate: 10,
+        count: 150
+    }
+}
 
 export function CreateProduct() {
     const [value, setValue] = useState('')
+    const [error, setError] = useState('')
 
-    const submitHandler = (event: React.FormEvent) => {
+    const submitHandler = async (event: React.FormEvent) => {
         event.preventDefault();
+        if (value.trim().length === 0) {
+            setError('Please enter valid title!')
+            return
+        }
+        productData.title = value
+        const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
     }
 
     // const changeHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -19,6 +41,7 @@ export function CreateProduct() {
                 placeholder="Enter product title..."
                 value={value}
                 onChange={(event) => setValue(event.target.value)} />
+            {error && <ErrorMessage error={error} />}
             <button type="submit" className="py-2 px-4 border bg-yellow-400 hover:text-white">Create</button>
         </form>
     )
