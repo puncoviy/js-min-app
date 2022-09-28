ec0d8fc76841b1a09c07cc41892de20527c08512
puncoviy <usaspinner@gmail.com>
2022-09-28 14:41:17 +0300

    feat: add create-product button

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { IProduct } from './models';
 
 function App() {
     const { loading, error, products, addProduct } = useProducts()
-    const [modal, setModal] = useState(true)
+    const [modal, setModal] = useState(false)
     const createHandler = (product: IProduct) => {
         setModal(false)
         addProduct(product)
@@ -20,9 +20,12 @@ function App() {
             {loading && <Loader />}
             {error && <ErrorMessage error={error} />}
             {products.map(product => <Product product={product} key={`item${product.id}`} />)}
-            { modal && <Modal title='Create new Product'>
+            { modal && <Modal title='Create new Product' onClose={() => setModal(false)}>
                 <CreateProduct onCreate={createHandler} />
             </Modal>}
+
+            <button className='absolute bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-2'
+            onClick={() => gitsetModal(true)}>+</button>
         </div>
     )
 }
