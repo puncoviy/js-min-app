d43e0a5fb3e587dedcf89a78fe241de5f4d49aaa
puncoviy <usaspinner@gmail.com>
2022-09-28 15:14:34 +0300

    feat: add router, release navigation

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,5 @@
+export function AboutPage() {
+  return (
+    <div>AboutPage Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur exercitationem eum minima maiores accusamus nisi, neque molestiae perspiciatis totam sed dicta doloremque expedita quos tenetur molestias optio enim adipisci praesentium architecto vel incidunt explicabo alias facere! Quae rem recusandae consectetur repellat similique suscipit dolorum ea illo, dicta a autem provident.</div>
+  )
+}
